8176483916bab3b9a9715c6936d47033cd5309d8
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-04-28 11:10:03 +0800

    type error in converting the string

--- processor_integer.py
+++ processor_integer.py
@@ -1,9 +1,17 @@
-with open("integer.txt", "r") as data:
+with open("integer.txt", "r") as data, \
+     open("double.txt", "w") as even_file, \
+     open("triple.txt", "w") as odd_file:
+
     for line in data:
         unsorted_num = int(line.strip())
         if unsorted_num % 2 == 0:
             answer = unsorted_num ** 2
+            str(answer + "\n")
+            even_file.write(answer)
         else:
             answer = unsorted_num ** 3
+            str(answer + "\n")
+            odd_file.write(answer)
+            
         print(answer)
 
