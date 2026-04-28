8b87964f6b07fb80a28a8f38fdca508db605aedb
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-04-28 11:23:37 +0800

    used user define

--- processor_integer.py
+++ processor_integer.py
@@ -1,17 +1,19 @@
-with open("integer.txt", "r") as data, \
+def process_even_odd_numbers():
+    with open("integer.txt", "r") as data, \
      open("double.txt", "w") as even_file, \
      open("triple.txt", "w") as odd_file:
 
-    for line in data:
-        unsorted_num = int(line.strip())
-        if unsorted_num % 2 == 0:
-            answer = unsorted_num ** 2
-            str(answer + "\n")
-            even_file.write(answer)
-        else:
-            answer = unsorted_num ** 3
-            str(answer + "\n")
-            odd_file.write(answer)
-            
-        print(answer)
+        for line in data:
+            unsorted_num = int(line.strip())
 
+            if unsorted_num % 2 == 0:
+                answer = unsorted_num ** 2
+                even_file.write(str(answer) + "\n")
+            else:
+                answer = unsorted_num ** 3
+                odd_file.write(str(answer) + "\n")
+            
+        print("successful")
+        return
+    
+process_even_odd_numbers()
