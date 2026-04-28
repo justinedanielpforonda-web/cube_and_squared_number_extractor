6816b0415a5449096caf616c0dcc3e62037d55ca
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-04-28 11:28:07 +0800

    added try catch

--- processor_integer.py
+++ processor_integer.py
@@ -1,19 +1,23 @@
 def process_even_odd_numbers():
-    with open("integer.txt", "r") as data, \
-     open("double.txt", "w") as even_file, \
-     open("triple.txt", "w") as odd_file:
+    try:
+        with open("integer.txt", "r") as data, \
+        open("double.txt", "w") as even_file, \
+        open("triple.txt", "w") as odd_file:
 
-        for line in data:
-            unsorted_num = int(line.strip())
+            for line in data:
+                unsorted_num = int(line.strip())
 
-            if unsorted_num % 2 == 0:
-                answer = unsorted_num ** 2
-                even_file.write(str(answer) + "\n")
-            else:
-                answer = unsorted_num ** 3
-                odd_file.write(str(answer) + "\n")
-            
-        print("successful")
-        return
-    
+                if unsorted_num % 2 == 0:
+                    answer = unsorted_num ** 2
+                    even_file.write(str(answer) + "\n")
+                else:
+                    answer = unsorted_num ** 3
+                    odd_file.write(str(answer) + "\n")
+                
+            print("successful")
+    except FileNotFoundError:
+            print("file not found")
+        
+            return
+        
 process_even_odd_numbers()
